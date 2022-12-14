123aba3c10fea894ce2e14c4234025bf95c4e0f4
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-14 17:24:13 -0500

    completed schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS roster_db;
+CREATE DATABASE roster_db;
+
+USE roster_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    tile VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+);
+
